712127f93e09d3fe72b35ce4fd31b20f64d3a43c
lucas.isensee2010@gmail.com <lucas.isensee2010@gmail.com>
2026-09-17 16:53:45 -0300

    Adicionando padrão unit of work

---
+++ Sincro.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sincro.Domain.Interfaces;
+
+namespace Sincro.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    public async Task SalvarAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
